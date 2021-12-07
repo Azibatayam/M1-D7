7fdf891d56c2a10a5dfa66f61d920f395ceab237
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2021-12-07 16:09:25 +0100

    Initial commit

---
+++ html.js
@@ -0,0 +1,72 @@
+//EX1: Add an h1 tag with the name of your shop
+
+<html> 
+    <head> 
+        <title>Teey Shop</title>
+        </head>
+        <body>
+            <h1>Teey Shop</h1>
+    body{
+        background: 
+    }
+     
+</html>
+ 
+ console.log(Teey Shop)
+
+
+ //EX2: Add a TABLE with the top 5 products of your shop 
+
+ <table>
+     <tr>
+ 
+         <th>Products List</th>
+
+         <th>Small Size</th>
+         <th>Medium Size</th>
+         <th>Large Size</th>
+     </tr>
+ </table>
+ <table>
+     <tr>
+         <td>Hat</td>
+         <td>5</td>
+         <td>7</td>
+         <td>0</td>
+     </tr>
+     
+     <tr>
+         <td>Shoes</td>
+         <td> 7</td>
+         <td>11</td>
+         <td>8</td>
+     </tr>
+     <tr>
+         <td>Trouser</td>
+         <td>10</td>
+         <td>9</td>
+         <td>6</td>
+     </tr>
+     <tr>
+         <td>Blouse</td>
+         <td>8</td>
+         <td>9</td>
+         <td>9</td>
+     </tr>
+     <tr>
+         <td>Shirt</td>
+         <td>8</td>
+         <td>5</td>
+         <td>3</td>
+     </tr>
+console.log(table)
+
+//*EX3: Insert for each product an image, a title, a description and a price
+
+     <li>
+         <img width="50" src= alt="logo"/>
+     Title: Shoe Description: Sports Price: 45 euro</li>
+
+     <li>
+         <img width="50" src="shoe.jpeg"alt="logo"/>
+     Title: Shoe Description: Sports Price: 45 euro</li>
